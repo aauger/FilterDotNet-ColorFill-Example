e241230253e01eb9e1df0e7918e5000589a082a1
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-02-14 23:05:44 -0500

    Commit initial project setup

---
+++ FDN-ColorFill/Program.cs
@@ -0,0 +1,7 @@
+﻿using FilterDotNet.Interfaces;
+using System.Drawing;
+
+// Initial test junk to ensure dependencies work.
+FastImageLibrary.FastImage myImage = new FastImageLibrary.FastImage(100, 100);
+IEngine engine = FastImageProvider.Injectables.FiEngine;
+Bitmap b = myImage.ToBitmap();
