3d5d9d195870acfefa167d5d1d02bc7830c9bfb4
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-02-15 20:31:21 -0500

    Create setup for layer inpainting algorithm

---
+++ FDN-ColorFill/LayerInpaintingFilter.cs
@@ -0,0 +1,37 @@
+﻿using FilterDotNet.Interfaces;
+
+namespace FDN_ColorFill
+{
+    public class LayerInpaintingFilter : IFilter, IConfigurableFilter
+    {
+        /* DI */
+        private readonly IPluginConfigurator<IImage> _pluginConfigurator;
+        private readonly IEngine _engine;
+
+        /* Internals */
+        private bool _ready = false;
+        private IImage _colorLayer;
+
+        /* Properties */
+        public string Name => "Layer In-Painting";
+
+        public LayerInpaintingFilter(IPluginConfigurator<IImage> pluginConfigurator, IEngine engine)
+        {
+            this._pluginConfigurator = pluginConfigurator;
+            this._engine = engine;
+        }
+
+        public IImage Apply(IImage input)
+        {
+            IImage output = this._engine.CreateImage(input.Width, input.Height);
+            throw new NotImplementedException();
+        }
+
+        public IFilter Initialize()
+        {
+            this._colorLayer = this._pluginConfigurator.GetPluginConfiguration();
+            this._ready = true;
+            return this;
+        }
+    }
+}
--- FDN-ColorFill/Program.cs
+++ FDN-ColorFill/Program.cs
@@ -1,7 +1,16 @@
 ﻿using FilterDotNet.Interfaces;
+using FilterDotNet.Filters;
 using System.Drawing;
+using FilterDotNet.LibraryConfigurators;
+using FDN_ColorFill;
 
-// Initial test junk to ensure dependencies work.
-FastImageLibrary.FastImage myImage = new FastImageLibrary.FastImage(100, 100);
 IEngine engine = FastImageProvider.Injectables.FiEngine;
-Bitmap b = myImage.ToBitmap();
+
+IImage image = engine.CreateImage(100, 100);
+
+IFilter thresholdFilter = new ThresholdFilter(new LambdaPluginConfigurator<int>(() => 127), engine).Initialize();
+IFilter minFilter = new StatisticalFilter(new LambdaPluginConfigurator<StatisticalFilterConfiguration>(
+    () => new() { Mode = StatisticalFilterMode.MIN, BlockSize = 8, Thresholding = false, Threshold = 0 }), engine).Initialize();
+IImage fPipeline = (new[] { minFilter, thresholdFilter }).Aggregate(image, (i, f) => f.Apply(i));
+IFilter layerInpaintingFilter = new LayerInpaintingFilter(new LambdaPluginConfigurator<IImage>(
+    () => fPipeline), engine).Initialize();
